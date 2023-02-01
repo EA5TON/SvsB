b1d5d2d405355faee8a41ac369aa60d98f961838
EA5TON <Lexa5Sk@rambler.ru>
2023-02-01 18:12:32 +0300

    Add.
    Win, Lose, Finish, value blocks.

---
+++ Assets/SVB/Scripts/Finish.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish: MonoBehaviour
+{
+    [SerializeField] GameObject Win;
+    [SerializeField] Controls Component_Controls;
+
+    void OnTriggerEnter ()
+    {
+        Component_Controls.Die = true;
+        Win.SetActive (true);
+    }
+
+    public void Button_Next_Level ()
+    {
+        Win.SetActive (false);
+    }
+
+    public void Button_Main_Menu ()
+    {
+        SceneManager.LoadScene (0);
+    }
+
+}
